92f12f87c35590a9b23eb1ba586044ece308aa14
Jeff <rustyangel@gmail.com>
2017-08-11 21:32:10 -0600

    🐛 Get color picker component working
    Mostly. Still not loading quite right, but builds work and it's functional.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
 <section class="bg-light-gray">
     <fieldset class="bn pa5 ma0">
-        <div v-for="(stop, index) in stops" v-bind:key="index">
-            <span class="f6 ttu tracked black-30">Colour {{ index + 1 }}</span>
+        <div class="mb4" v-for="(stop, index) in stops" v-bind:key="index">
+            <span class="dib mb2 f6 ttu tracked black-30">Colour {{ index + 1 }}</span>
             <color-picker v-model="stop.color"></color-picker>
             <!-- <input class="db" type="color" v-model="stop.color"> -->
         </div>
@@ -13,13 +13,19 @@
 </template>
 
 <script>
-import Chrome from 'vue-color/src/components/Chrome.vue'
+// import ColorPicker from 'vue-color/src/components/Chrome.vue'
 import Gradient from '~components/Gradient.vue'
+
+let ColorPicker
+
+if (process.BROWSER_BUILD) {
+    ColorPicker = require('vue-color/dist/vue-color.min.js').Chrome
+}
 
 
 export default {
     components: {
-        'color-picker': Chrome,
+        ColorPicker,
         Gradient,
     },
 
