761ed6b0776223a5861953c3a3068934db3c73be
Jeff <rustyangel@gmail.com>
2017-08-22 10:02:20 -0600

    👕 Install prettier & eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,12 @@ module.exports = {
     },
     extends: 'standard',
     plugins: [
+        'prettier',
         'html'
     ],
     rules: {
+        "prettier/prettier": "error",
+
         'arrow-parens': 0,  // allow paren-less arrow functions
         'generator-star-spacing': 0,  // allow async-await
         'no-console': 2,  // do not allow console.logs etc.
