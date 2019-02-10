eb65e762535d7ac4bb789b5f9b088cc387ba77ca
Jeff <rustyangel@gmail.com>
2019-02-10 12:46:28 -0700

    Add ColorField component

---
+++ components/ColorField.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+        <v-input
+            :value="hex"
+            class="color-hex f4"
+            type="text"
+            @input="updateHex($event.target.value)"
+        />
+        <span class="dib mt3 f7 ttu tracked black-30">Hue {{ parseInt(hue) }}º</span>
+        <range-field class="mt1" :value="hue" :min="0" :max="360" @input="updateHue"></range-field>
+        <span class="dib mt2 f7 ttu tracked black-30">Saturation {{ saturation }}%</span>
+        <range-field
+            class="mt1"
+            :value="saturation"
+            :min="0"
+            :max="1"
+            :step="0.01"
+            @input="updateSaturation"
+        ></range-field>
+        <span class="dib mt2 f7 ttu tracked black-30">Lightness {{ lightness }}%</span>
+        <range-field
+            class="mt1"
+            :value="lightness"
+            :min="0"
+            :max="1"
+            :step="0.01"
+            @input="updateLightness"
+        ></range-field>
+    </div>
+</template>
+
+<script>
+import chroma from 'chroma-js'
+
+import VInput from './VInput.vue'
+import RangeField from './RangeField.vue'
+
+export default {
+    components: {
+        VInput,
+        RangeField,
+    },
+
+    model: {
+        prop: 'color',
+        event: 'update',
+    },
+
+    props: {
+        color: {
+            type: String,
+            default: '',
+        }
+    },
+
+    data () {
+        return {
+            hex: this.color,
+        }
+    },
+
+    computed: {
+        hue: {
+            get () {
+                // Ensure this doesn't return NaN.
+                return chroma(this.hex).get('hsl.h') || 0
+            },
+            set (hue) {
+                this.updateHex(chroma(hue, this.saturation, this.lightness, 'hsl').hex())
+            },
+        },
+
+        saturation: {
+            get () {
+                return (chroma(this.hex).get('hsl.s'))
+            },
+            set (saturation) {
+                this.updateHex(chroma(this.hue, saturation, this.lightness, 'hsl').hex())
+            },
+        },
+
+        lightness: {
+            get () {
+                return (chroma(this.hex).get('hsl.l'))
+            },
+            set (lightness) {
+                this.updateHex(chroma(this.hue, this.saturation, lightness, 'hsl').hex())
+            },
+        }
+    },
+
+    methods: {
+        setColor (color) {
+            this.$emit('update', color)
+        },
+
+        updateHex (hex) {
+            if (hex !== this.hex) {
+                this.hex = hex
+                this.setColor(hex)
+            }
+        },
+
+        updateHue (hue) {
+            this.hue = hue
+            this.updateHex(chroma(this.hue, this.saturation, this.lightness, 'hsl').hex())
+        },
+
+        updateSaturation (saturation) {
+            this.saturation = saturation
+            this.updateHex(chroma(this.hue, this.saturation, this.lightness, 'hsl').hex())
+        },
+
+        updateLightness (lightness) {
+            this.lightness = lightness
+            this.updateHex(chroma(this.hue, this.saturation, this.lightness, 'hsl').hex())
+        },
+    },
+}
+</script>
+
+<style>
+.color-hex { outline: 0; }
+</style>
+
---
+++ components/VInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <input
+        :type="type"
+        :class="$style.input"
+        :value="value"
+        @input="$emit('update', $event.target.value)"
+        v-on="$listeners"
+    >
+</template>
+
+<script>
+export default {
+  model: {
+    event: 'update',
+  },
+
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+}
+</script>
+
+<style module>
+.input {
+  display: block;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  font-family: 'SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace;
+  line-height: 1;
+  border: 0;
+  border-radius: 0;
+}
+</style>
