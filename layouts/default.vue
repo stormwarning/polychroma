58cc273c58aecdcf0f84d4015869e6a157490be4
Jeff <rustyangel@gmail.com>
2019-02-14 14:02:36 -0700

    Move header & add nav to layout template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,57 @@
 <template>
-    <main class="min-vh-100 sans-serif">
-        <nuxt />
+    <article class="min-vh-100 sans-serif">
+        <header class="absolute right-2 pt4">
+            <h1 class="mb0 f3 f2-ns f1-l ttu tracked-mega">Polychroma</h1>
+            <span class="db b tr f6 f5-ns">v{{ version }}</span>
+        </header>
+        <main>
+            <nav>
+                <div class="measure center">
+                    <nuxt-link
+                        to="/"
+                        exact
+                        class="dib pb3 f6 b ttu tracked black-30 no-underline bb bw2 b--black"
+                    >
+                        Controls
+                    </nuxt-link>
+                    <nuxt-link
+                        to="/about"
+                        class="dib pb3 f6 b ttu tracked black-30 no-underline bb bw2 b--black"
+                    >
+                        About
+                    </nuxt-link>
+                </div>
+            </nav>
+            <section>
+                <nuxt />
+            </section>
+            <figure></figure>
+        </main>
         <page-footer />
-    </main>
+    </article>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import PageFooter from '~/components/Footer.vue'
 
 export default {
     components: {
         PageFooter,
     },
+
+    computed: {
+        ...mapState({
+            version: (state) => state.version,
+            direction: (state) => state.direction,
+        }),
+    },
 }
 </script>
 
 <style scoped>
-main {
+article {
     display: grid;
     grid-template-areas: 'main' 'footer';
     grid-template-rows: 1fr 120px;
@@ -24,10 +59,38 @@ main {
 }
 
 @media screen and (min-width: 60em) {
-    main {
+    article {
         grid-template-areas: 'main sidebar';
         grid-template-rows: auto;
         grid-template-columns: 1fr 240px;
     }
+}
+
+main {
+    display: grid;
+    grid-area: main;
+    grid-template-areas: 'result' 'navbar' 'content';
+    grid-template-rows: 33vh 4rem 1fr;
+    grid-template-columns: 1fr;
+}
+
+@media screen and (min-width: 60em) {
+    main {
+        grid-template-areas: 'navbar result' 'content result';
+        grid-template-rows: 8.5rem auto;
+        grid-template-columns: 1fr 1fr;
+    }
+}
+
+main > section {
+    grid-area: content;
+}
+
+nav {
+    grid-area: navbar;
+}
+
+figure {
+    grid-area: result;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,104 +1,96 @@
 <template>
-    <section class="body-section">
-        <header class="absolute right-2 pt4">
-            <h1 class="mb0 f3 f2-ns f1-l ttu tracked-mega">Polychroma</h1>
-            <span class="db b tr f6 f5-ns">v{{ version }}</span>
-        </header>
+    <form class="pa4 pa5-ns ma0">
+        <fieldset class="pa0 ma0 bn measure center">
+            <legend class="f6 ttu tracked black-30 ma0">
+                Colour Mode
+            </legend>
 
-        <form class="gradient-controls pa4 pa5-ns ma0">
-            <fieldset class="pa0 ma0 mb5 bn measure center">
-                <legend class="f6 ttu tracked black-30 mb2">Colour Mode</legend>
-
-                <div
-                    class="drop-shadow flex items-center pa3 pa4-ns bg-white br1 overflow-hidden"
+            <div
+                class="drop-shadow flex items-center pa3 pa4-ns mt2 bg-white br1 overflow-hidden"
+            >
+                <label
+                    v-for="(m, index) in modes"
+                    :key="index"
+                    class="flex items-center mv2 mr3 lh-solid pointer"
                 >
-                    <label
-                        v-for="(m, index) in modes"
-                        :key="index"
-                        class="flex items-center mv2 mr3 lh-solid pointer"
-                    >
-                        <input
-                            v-model="mode"
-                            :value="m.toLowerCase()"
-                            class="mr2"
-                            type="radio"
-                            name="mode"
+                    <input
+                        v-model="mode"
+                        :value="m.toLowerCase()"
+                        class="mr2"
+                        type="radio"
+                        name="mode"
+                    />
+                    <span class="f7 f6-ns tracked">{{ m }}</span>
+                </label>
+            </div>
+        </fieldset>
+
+        <fieldset class="pa0 ma0 mt5 bn measure center">
+            <legend class="f6 ttu tracked black-30 ma0">
+                Gradient Options
+            </legend>
+
+            <section class="drop-shadow gradient-options mt2 bg-white br1">
+                <option-controls>
+                    <template slot="summary">
+                        <span class="dib f7 f6-ns ttu tracked black-30">
+                            Direction
+                        </span>
+                        <svg
+                            class="w1 h1"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                :transform="rotation"
+                                d="M10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10zm1-18H9v7h2V2z"
+                                fill-rule="nonzero"
+                                fill="#000"
+                                fill-opacity="0"
+                            ></path>
+                            <path
+                                :transform="rotation"
+                                d="M9 2.0619C5.0537 2.554 2 5.92037 2 10c0 4.41828 3.58172 8 8 8s8-3.58172 8-8c0-4.07962-3.0537-7.446-7-7.9381V9H9V2.0619zM10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10z"
+                                fill-rule="nonzero"
+                                fill="#000"
+                                fill-opacity="1"
+                            ></path>
+                        </svg>
+                    </template>
+                    <range-field
+                        :value="direction"
+                        :min="0"
+                        :max="360"
+                        @input="rotateGradient"
+                    />
+                </option-controls>
+                <option-controls
+                    v-for="(stop, index) in stops"
+                    :key="index"
+                    class="bt b--black-10"
+                >
+                    <template slot="summary">
+                        <span class="dib f7 f6-ns ttu tracked black-30">
+                            <b v-if="index === 0" class="normal">Start</b>
+                            <b v-else class="normal">End</b>
+                            Colour
+                        </span>
+                        <span
+                            :style="{ backgroundColor: stop.color.hex }"
+                            class="dib w1 h1 br-pill"
                         />
-                        <span class="f7 f6-ns tracked">{{ m }}</span>
-                    </label>
-                </div>
-            </fieldset>
-
-            <fieldset class="pa0 ma0 bn measure center">
-                <legend class="f6 ttu tracked black-30 mb2">
-                    Gradient Options
-                </legend>
-
-                <section class="drop-shadow gradient-options bg-white br1">
-                    <option-controls>
-                        <template slot="summary">
-                            <span class="dib f7 f6-ns ttu tracked black-30">
-                                Direction
-                            </span>
-                            <svg
-                                class="w1 h1"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    :transform="rotation"
-                                    d="M10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10zm1-18H9v7h2V2z"
-                                    fill-rule="nonzero"
-                                    fill="#000"
-                                    fill-opacity="0"
-                                ></path>
-                                <path
-                                    :transform="rotation"
-                                    d="M9 2.0619C5.0537 2.554 2 5.92037 2 10c0 4.41828 3.58172 8 8 8s8-3.58172 8-8c0-4.07962-3.0537-7.446-7-7.9381V9H9V2.0619zM10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10z"
-                                    fill-rule="nonzero"
-                                    fill="#000"
-                                    fill-opacity="1"
-                                ></path>
-                            </svg>
-                        </template>
-                        <range-field
-                            :value="direction"
-                            :min="0"
-                            :max="360"
-                            @input="rotateGradient"
-                        />
-                    </option-controls>
-                    <option-controls
-                        v-for="(stop, index) in stops"
-                        :key="index"
-                        class="bt b--black-10"
-                    >
-                        <template slot="summary">
-                            <span class="dib f7 f6-ns ttu tracked black-30">
-                                <b v-if="index === 0" class="normal">Start</b>
-                                <b v-else class="normal">End</b>
-                                Colour
-                            </span>
-                            <span
-                                :style="{ backgroundColor: stop.color.hex }"
-                                class="dib w1 h1 br-pill"
-                            />
-                        </template>
-                        <color-field v-model="stop.color.hex" />
-                    </option-controls>
-                </section>
-            </fieldset>
-        </form>
-
-        <gradient :stops="stops" :mode="mode" :dir="direction" />
-    </section>
+                    </template>
+                    <color-field v-model="stop.color.hex" />
+                </option-controls>
+            </section>
+        </fieldset>
+    </form>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
-import Gradient from '~/components/Gradient.vue'
 import ColorField from '~/components/ColorField.vue'
 import RangeField from '~/components/RangeField.vue'
 import OptionControls from '~/components/OptionControls.vue'
@@ -106,15 +98,12 @@ import OptionControls from '~/components/OptionControls.vue'
 export default {
     components: {
         OptionControls,
-        Gradient,
         ColorField,
         RangeField,
     },
 
     data() {
         return {
-            version: process.env.VERSION,
-
             stops: [
                 {
                     color: {
@@ -140,6 +129,7 @@ export default {
         },
 
         ...mapState({
+            version: (state) => state.version,
             direction: (state) => state.direction,
         }),
     },
@@ -153,26 +143,6 @@ export default {
 </script>
 
 <style scoped>
-.body-section {
-    display: grid;
-    grid-area: main;
-    grid-template-areas: 'result' 'controls';
-    grid-template-rows: 33vh 1fr;
-    grid-template-columns: 1fr;
-}
-
-@media screen and (min-width: 60em) {
-    .body-section {
-        grid-template-areas: 'controls result';
-        grid-template-rows: auto;
-        grid-template-columns: 1fr 1fr;
-    }
-}
-
-.gradient-controls {
-    grid-area: controls;
-}
-
 .drop-shadow {
     box-shadow: rgba(0, 0, 0, 0.12) 0 2px 10px,
         0 20px 50px 0 rgba(0, 0, 0, 0.14);
