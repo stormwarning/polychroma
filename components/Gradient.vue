09f87feae7687184ef1d562f64f6591134af96c5
Jeff <rustyangel@gmail.com>
2020-04-17 22:09:30 -0600

    Update GradientResult layout

--- components/Gradient.vue
+++ components/Gradient.vue
@@ -1,52 +1,64 @@
 <template>
-    <figure
-        :style="{ backgroundImage: gradientCSS }"
-        class="flex items-center justify-center pa5 ma0"
-    >
-        <div class="dn db-l w-100 mw6 bg-white br1 overflow-hidden shadow-b">
-            <button
-                class="button-reset w-100 pa4 f7 f7-ns ttu tracked lh-solid black-90 bg-white ba bw2 b--white tl outline-0 pointer hide-child relative"
-                @click="copyCSS(gradientCSS)"
-            >
-                <div
-                    :style="{ backgroundImage: gradientCSS }"
-                    class="button-bg child absolute absolute--fill"
-                />
-                <div
-                    class="button-text absolute absolute--fill flex items-center justify-center ma1 bg-white"
-                >
-                    <svg
-                        class="relative w1 h1 mr2 v-mid"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="#000"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    >
-                        <path
-                            d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
-                        ></path>
-                        <rect
-                            x="8"
-                            y="2"
-                            width="8"
-                            height="4"
-                            rx="1"
-                            ry="1"
-                        ></rect>
-                    </svg>
-                    <span class="relative lh-solid v-mid">
-                        {{ copyButtonText }}
-                    </span>
-                </div>
-            </button>
-            <pre class="source-code ph4 pv5 ma0 ttl ws-normal bg-light-gray">
+    <figure :style="{ backgroundImage: gradientCSS }" class="ma0">
+        <div class="dn db-l w-100 mw6">
+            <pre :class="$style.source" class="ph4 pv5 ma0 f6 ttl ws-normal">
                 <code>{{ gradientCSS }}</code>
             </pre>
+        </div>
+
+        <div class="flex items-end justify-end ph4 pv5">
+            <button
+                class="button-reset pa3 f7 f7-ns ttu tracked lh-solid black-90 bg-white bn tl outline-0 pointer hide-child relative"
+                @click="copyCSS(gradientCSS)"
+            >
+                <svg
+                    class="relative w1 h1 v-mid"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#000"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <path
+                        d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
+                    ></path>
+                    <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+                </svg>
+                <span class="relative ml2 lh-solid v-mid">
+                    {{ copyButtonText }}
+                </span>
+            </button>
+            <button
+                class="button-reset pa3 ml3 f7 f7-ns ttu tracked lh-solid black-90 bg-white bn tl outline-0 pointer hide-child relative"
+                @click="shareURL"
+            >
+                <svg
+                    class="relative w1 h1 v-mid"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <path
+                        d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
+                    ></path>
+                    <path
+                        d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
+                    ></path>
+                </svg>
+                <span class="relative ml2 lh-solid v-mid">
+                    Share
+                </span>
+            </button>
         </div>
     </figure>
 </template>
@@ -55,7 +67,7 @@
 import chroma from 'chroma-js'
 import { mapState } from 'vuex'
 
-import { copyTextToClipboard } from '../utils/clipboard'
+import { copyTextToClipboard } from '~/utils/clipboard'
 
 export default {
     data() {
@@ -109,12 +121,35 @@ export default {
                 this.copyButtonText = 'Copy CSS'
             }, 2000)
         },
+
+        shareURL() {
+            let mode = this.mode
+            let deg = this.dir
+            let stops = this.stops
+            let start = stops[0].color.hex.replace('#', '')
+            let end = stops[stops.length - 1].color.hex.replace('#', '')
+            let str = `/mode/${mode}/start/${start}/end/${end}/angle/${deg}`
+
+            console.log(str)
+        },
     },
 }
 </script>
 
-<style lang="postcss">
-.source-code {
-    line-height: 2;
+<style lang="postcss" module>
+figure {
+    display: grid;
+    grid-template-rows: auto;
+    grid-template-columns: 1fr;
+    padding-top: 8.5rem;
+}
+
+.source {
+    mix-blend-mode: color-burn;
+    opacity: 0.75;
+
+    code {
+        line-height: 2;
+    }
 }
 </style>
