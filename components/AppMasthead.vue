a862b3ab5bc46412ce7f1585249328a7c3113d79
Jeff <rustyangel@gmail.com>
2021-03-31 21:06:19 -0600

    Fix sticky header

--- components/AppMasthead.vue
+++ components/AppMasthead.vue
@@ -1,12 +1,15 @@
 <template>
-    <header class="absolute top-0 right-8 z-10 pt-8">
+    <header class="absolute top-0 right-8 z-10 pt-20">
         <h1
-            class="text-2xl sm:text-4xl lg:text-5xl font-bold uppercase tracking-mega"
+            class="text-2xl sm:text-4xl lg:text-5xl font-bold leading-none uppercase tracking-mega capsize"
             >Polychroma</h1
         >
-        <span class="block font-bold text-right text-sm sm:text-base"
-            >v{{ version }}</span
-        >
+        <div class="mt-4">
+            <span
+                class="block font-bold text-right text-sm sm:text-base leading-none capsize"
+                >v{{ version }}</span
+            >
+        </div>
     </header>
 </template>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,12 @@
 <template>
     <article class="min-h-screen">
-        <div class="sticky">
+        <div
+            class="header sticky top-0 z-10 sm:h-screen pointer-events-none mix-burn"
+        >
             <app-masthead class="mix-burn" />
         </div>
         <main>
-            <nav class="p-8">
-                <div class="flex gap-4 max-w-md mx-auto">
-                    <nuxt-link
-                        to="/"
-                        exact
-                        class="py-2 text-sm font-semibold text-gray-500 uppercase tracking-widest no-underline leading-4 capsize"
-                    >
-                        Controls
-                    </nuxt-link>
-                    <nuxt-link
-                        to="/info"
-                        class="py-2 text-sm font-semibold text-gray-500 uppercase tracking-widest no-underline leading-4 capsize"
-                    >
-                        Info
-                    </nuxt-link>
-                </div>
-            </nav>
+            <app-nav />
             <section class="p-8 sm:pt-0 sm:pb-16">
                 <nuxt />
             </section>
@@ -33,15 +19,17 @@
 <script>
 import { mapState } from 'vuex'
 
-import AppMasthead from '~/components/AppMasthead'
-import GradientResult from '~/components/Gradient.vue'
 import AppFooter from '~/components/AppFooter'
+import AppMasthead from '~/components/AppMasthead'
+import AppNav from '~/components/AppNav.vue'
+import GradientResult from '~/components/Gradient.vue'
 
 export default {
     components: {
         AppFooter,
         AppMasthead,
         GradientResult,
+        AppNav,
     },
 
     computed: {
@@ -60,7 +48,7 @@ article {
     grid-template-columns: 1fr;
 }
 
-@media screen and (min-width: 60em) {
+@screen lg {
     article {
         grid-template-areas: 'main sidebar';
         grid-template-rows: auto;
@@ -76,7 +64,7 @@ main {
     grid-template-columns: 1fr;
 }
 
-@media screen and (min-width: 60em) {
+@screen lg {
     main {
         grid-template-areas: 'navbar result' 'content result';
         grid-template-rows: 8.5rem auto;
@@ -94,5 +82,12 @@ nav {
 
 figure {
     grid-area: result;
+}
+
+.header {
+    @screen lg {
+        grid-area: sidebar;
+        grid-row: 1;
+    }
 }
 </style>
