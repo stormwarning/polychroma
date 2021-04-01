7b6c0602374912f50e8425d090c2e18472fabb91
Jeff <rustyangel@gmail.com>
2021-03-31 21:05:37 -0600

    Split nav elements into separate components

---
+++ components/AppNavLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <nuxt-link :to="to" :exact="exact" class="py-2 text-gray-500 no-underline">
+        <span
+            class="text-sm font-semibold leading-4 uppercase tracking-widest capsize"
+        >
+            <slot />
+        </span>
+    </nuxt-link>
+</template>
+
+<script>
+export default {
+    props: {
+        to: {
+            type: String,
+            default: '/',
+        },
+        exact: {
+            type: Boolean,
+            default: false,
+        },
+    },
+}
+</script>
