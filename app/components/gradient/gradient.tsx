d1d2bf2923a1148a78a565354061090897c99446
Jeff <rustyangel@gmail.com>
2023-06-17 21:55:00 -0600

    Add Copy and Link icons

--- app/components/gradient/gradient.tsx
+++ app/components/gradient/gradient.tsx
@@ -1,5 +1,7 @@
 import { Box } from '../box/box'
 import { Button } from '../button/button'
+import { IconCopy } from '../icons/icon-copy'
+import { IconLink } from '../icons/icon-link'
 
 import { useGradientState } from './gradient-provider'
 
@@ -49,48 +51,11 @@ export function Gradient() {
 				paddingX={32}
 				paddingY={64}
 			>
-				<Button onClick={copyCSS}>
-					<svg
-						width="16"
-						height="16"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
-						<rect
-							x="8"
-							y="2"
-							width="8"
-							height="4"
-							rx="1"
-							ry="1"
-						></rect>
-					</svg>
-					<span className="relative ml-1 lg:ml-2 leading-none">
-						{copyButtonText}
-					</span>
+				<Button onClick={copyCSS} icon={<IconCopy />}>
+					{copyButtonText}
 				</Button>
-				<Button onClick={shareURL}>
-					<svg
-						width="16"
-						height="16"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-						<path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
-					</svg>
-					<span className="relative ml-1 lg:ml-2 leading-none">
-						{shareButtonText}
-					</span>
+				<Button onClick={shareURL} icon={<IconLink />}>
+					{shareButtonText}
 				</Button>
 			</Box>
 		</Box>
