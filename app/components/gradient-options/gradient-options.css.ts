e040c827a9e43c6c50aacd1b0a7087b0bd5c4364
Jeff <rustyangel@gmail.com>
2023-06-20 10:04:51 -0600

    Add inset shadow to colour swatch

--- app/components/gradient-options/gradient-options.css.ts
+++ app/components/gradient-options/gradient-options.css.ts
@@ -5,4 +5,5 @@ export const swatchBackground = createVar()
 export const swatch = style({
 	backgroundColor: swatchBackground,
 	borderRadius: '9999px',
+	boxShadow: 'inset 0 0 0 1.5px rgba(0 0 0 / 50%)',
 })
